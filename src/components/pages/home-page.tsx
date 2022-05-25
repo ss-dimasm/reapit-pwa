8261ffdaabaa80451465ca4720af302adfb299a4
Dimas M <dimas.m@softwareseni.com>
2022-05-25 13:46:38 +0700

    chore: text

--- src/components/pages/home-page.tsx
+++ src/components/pages/home-page.tsx
@@ -1,11 +1,13 @@
 import React, { FC } from 'react'
 
 import { PageContainer, Title } from '@reapit/elements'
+import { useDetectPWA } from 'utils/hooks/useDetectPWA'
 
 export const HomePage: FC = () => {
+  const { isInsidePWA } = useDetectPWA()
   return (
     <PageContainer>
-      <Title>dimasm, hi from page</Title>
+      <Title>Hi dimasm, your&apos;re running {isInsidePWA ? 'inside' : 'outside'} PWA</Title>
     </PageContainer>
   )
 }
